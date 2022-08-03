2d56334869ec5b28efca9655619c2659ccd64af3
Pedro Lucas <pdrolucas.dev@gmail.com>
2022-08-03 09:09:38 -0300

    feat: atualizado estilos de media

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,20 @@
 import axios from "axios";
 import Head from "next/head";
+
 import { useState } from "react";
 
 export default function Home() {
   const [value, setValue] = useState("");
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState([]);
 
   const getUser = async (username) => {
     const { data } = await axios.post("/api/user", {
       username,
     });
-    console.log(data);
-    setUser(() => data);
+    setUser(() => data.data.business_discovery.media);
     console.log(user);
   };
+
   return (
     <>
       <Head>
@@ -25,21 +26,36 @@ export default function Home() {
       <main>
         <section className="lg:container flex items-center justify-center h-screen">
           <div className="text-center">
-            <h1 className="font-extrabold text-xl mb-12">Search User</h1>
+            <h1 className="font-extrabold text-2xl mb-12">Search User</h1>
             <input
-              className="bg-zinc-800 rounded-full py-2 px-4 border-none"
+              className="bg-zinc-800 rounded-l-full py-2 px-4 border-none"
               type="text"
               placeholder="Digite o nome do usuário"
               value={value}
               onChange={(e) => setValue(e.target.value)}
             />
             <button
-              className="bg-blue-500 ml-4 px-4 py-2 rounded-md"
+              className="bg-blue-500 px-4 py-2 rounded-r-full"
               onClick={() => getUser(value)}
               disabled={!value}
             >
               Search
             </button>
+          </div>
+        </section>
+
+        {/* <section>{user ? <p>{user.data[0].id}</p> : <h1>N</h1>}</section> */}
+        <section className="container">
+          <div className="flex">
+            {user.length > 0 ? (
+              user.data.map((value) => (
+                <div key={value.id}>
+                  <img src={value.media_url} alt="ig_image" width={300} />
+                </div>
+              ))
+            ) : (
+              <h1>Nada</h1>
+            )}
           </div>
         </section>
       </main>
